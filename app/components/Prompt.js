dff66530bf714497001624efbf05cbb4a36194ef
Mark Berry <mark@Marks-MacBook-Air.local>
2017-01-25 11:23:15 -0600

    Turn Prompt into a stateless functional component

--- app/components/Prompt.js
+++ app/components/Prompt.js
@@ -2,40 +2,38 @@ var React = require('react');
 var PropTypes = React.PropTypes;
 var transparentBg = require('../styles').transparentBg;
 
-var Prompt = React.createClass({
-  propTypes: {
+var Prompt = function(props) {
+  return (
+    <div className="jumbotron col-sm-6 col-sm-offset-3 text-center" style={transparentBg}>
+      <h1>{props.header}</h1>
+      <div className="col-sm-12">
+        <form onSubmit={props.onSubmitUser} >
+            <div className="form-group">
+              <input
+                className="form-control"
+                placeholder="Github Username"
+                value={props.username}
+                onChange={props.onUpdateUser}
+                type="text" />
+            </div>
+            <div className="col-sm-4 col-sm-offset-4 text-center">
+              <button
+                className="btn btn-block btn-success"
+                type="submit">
+                Continue
+              </button>
+            </div>
+        </form>
+      </div>
+    </div>
+  )
+};
+
+Prompt.propTypes = {
     username : PropTypes.string.isRequired,
     header: PropTypes.string.isRequired,
     onSubmitUser: PropTypes.func.isRequired,
     onUpdateUser: PropTypes.func.isRequired
   },
-
-  render: function() {
-    return (
-        <div className="jumbotron col-sm-6 col-sm-offset-3 text-center" style={transparentBg}>
-          <h1>{this.props.header}</h1>
-          <div className="col-sm-12">
-            <form onSubmit={this.props.onSubmitUser} >
-                <div className="form-group">
-                  <input
-                    className="form-control"
-                    placeholder="Github Username"
-                    value={this.props.username}
-                    onChange={this.props.onUpdateUser}
-                    type="text" />
-                </div>
-                <div className="col-sm-4 col-sm-offset-4 text-center">
-                  <button
-                    className="btn btn-block btn-success"
-                    type="submit">
-                    Continue
-                  </button>
-                </div>
-            </form>
-          </div>
-        </div>
-      )
-  }
-});
 
 module.exports = Prompt;
