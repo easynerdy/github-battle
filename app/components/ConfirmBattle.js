bde2509ee96f13fdc40a0c8e3e20ada37a7eaaad
Mark Berry <mark@Marks-MacBook-Air.local>
2017-01-31 12:11:04 -0600

    Add skeleton for ConfirmBattle page

---
+++ app/components/ConfirmBattle.js
@@ -0,0 +1,9 @@
+var React = require('React');
+
+var ConfirmBattle = React.createClass({
+  render : function() {
+    return <div>Confirm Battle!!! </div>
+  }
+});
+
+module.exports = ConfirmBattle;
